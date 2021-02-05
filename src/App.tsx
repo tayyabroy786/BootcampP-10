3b320f1fb69af5d5abd759768c4b4b3a19394245
tayyabroy786 <30634558+tayyabroy786@users.noreply.github.com>
2021-02-06 01:47:56 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StepperComponent from './Stepper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StepperComponent />
+    </div>
+  );
+}
+
+export default App;
